56a58bad3055713f7c86803128e8b51f9f138f70
user <offonoff6101@naver.com>
2025-11-16 23:56:14 +0900

    feat(model): Add SimulationState entity

--- src/main/java/com/example/chimpanzee_simulation/domain/model/SimulationState.java
+++ src/main/java/com/example/chimpanzee_simulation/domain/model/SimulationState.java
@@ -1,4 +1,12 @@
 package com.example.chimpanzee_simulation.domain.model;
 
+import java.util.List;
+
 public class SimulationState {
+
+    private int turnNumber;                     // 현재 턴 번호
+    private List<Chimpanzee> chimpanzees;
+    private Environment environment;
+
+    private long randomSeed;        // 재현성을 위해 필요하면 사용
 }
