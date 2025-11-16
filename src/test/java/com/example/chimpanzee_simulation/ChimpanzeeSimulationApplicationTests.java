357bd010f470331ac0ed7b8a3f42de54856fe383
박준범_T8083 <ttotto0421@gmail.com>
2025-11-16 22:55:05 +0900

    chore: initialize project structure

---
+++ src/test/java/com/example/chimpanzee_simulation/ChimpanzeeSimulationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chimpanzee_simulation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChimpanzeeSimulationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
