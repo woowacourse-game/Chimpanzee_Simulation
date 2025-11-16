37c15702e126ba405a6cd4d82e4ff48b972386f6
user <offonoff6101@naver.com>
2025-11-16 23:51:53 +0900

    feat(Model): Add Environment entity with food, weather and dangerLevel

--- src/main/java/com/example/chimpanzee_simulation/domain/model/Environment.java
+++ src/main/java/com/example/chimpanzee_simulation/domain/model/Environment.java
@@ -1,4 +1,10 @@
 package com.example.chimpanzee_simulation.domain.model;
 
+import com.example.chimpanzee_simulation.domain.enums.Weather;
+
 public class Environment {
+
+    private int food;               // 한 턴에 생산된 먹이 수량
+    private Weather weather;        // enum Weather { SUNNY, NORMAL, RAINY, STORM, DROUGHT}
+    private double dangerLevel;     // 0.0 ~ 1.0 위험도
 }
