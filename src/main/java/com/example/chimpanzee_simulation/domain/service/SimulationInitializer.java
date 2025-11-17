9e6e8ec38692ef9503607ac03b92b46ca6646cfc
user <offonoff6101@naver.com>
2025-11-17 23:06:22 +0900

    feat(domain): Add SimulationInitializer and its implementation

---
+++ src/main/java/com/example/chimpanzee_simulation/domain/service/SimulationInitializer.java
@@ -0,0 +1,4 @@
+package com.example.chimpanzee_simulation.domain.service;
+
+public interface SimulationInitializer {
+}
---
+++ src/main/java/com/example/chimpanzee_simulation/domain/service/SimulationInitializerImpl.java
@@ -0,0 +1,4 @@
+package com.example.chimpanzee_simulation.domain.service;
+
+public class SimulationInitializerImpl {
+}
