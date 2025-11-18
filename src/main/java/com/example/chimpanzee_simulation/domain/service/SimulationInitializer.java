07c011c59bd89aa4c7373e6fc3c3bd7ceac485d3
user <offonoff6101@naver.com>
2025-11-19 01:24:08 +0900

    feat(domain): Implement createInitialState for initializing chimp population and environment

--- src/main/java/com/example/chimpanzee_simulation/domain/service/SimulationInitializer.java
+++ src/main/java/com/example/chimpanzee_simulation/domain/service/SimulationInitializer.java
@@ -1,4 +1,8 @@
 package com.example.chimpanzee_simulation.domain.service;
 
+import com.example.chimpanzee_simulation.domain.model.SimulationState;
+
 public interface SimulationInitializer {
+
+    SimulationState createInitialState();
 }
--- src/main/java/com/example/chimpanzee_simulation/domain/service/SimulationInitializerImpl.java
+++ src/main/java/com/example/chimpanzee_simulation/domain/service/SimulationInitializerImpl.java
@@ -1,6 +1,8 @@
 package com.example.chimpanzee_simulation.domain.service;
 
 import com.example.chimpanzee_simulation.domain.model.Chimpanzee;
+import com.example.chimpanzee_simulation.domain.model.Environment;
+import com.example.chimpanzee_simulation.domain.model.SimulationState;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,16 +12,24 @@ public class SimulationInitializerImpl implements SimulationInitializer {
 
     private static final int DEFAULT_INITIAL_POPULATION = 20;
 
-    private static final int MIN_LONGEVITY = 36;
-    private static final int MAX_LONGEVITY = 45;
+    public SimulationState createInitialState() {
+        long seed = System.currentTimeMillis();
+        Random random = new Random(seed);
 
-    private static final int INITIAL_FOOD = 100;
-
-    private List<Chimpanzee> createInitialChimpanzees(int count, Random random) {
-        List<Chimpanzee> chimpanzeeList = new ArrayList<>();
-
-        for (int i = 0; i < count; i++) {
-
+        List<Chimpanzee> chimpanzees = new ArrayList<>();
+        for (int i =0; i < DEFAULT_INITIAL_POPULATION; i++) {
+            chimpanzees.add(Chimpanzee.randomInitial((long) i+1, random));
         }
+
+        Environment environment = Environment.randomInitial(DEFAULT_INITIAL_POPULATION, random);
+
+        SimulationState state = new SimulationState(
+                0,
+                chimpanzees,
+                environment,
+                seed
+        );
+
+        return state;
     }
 }
